52b35ce8b5b421552598380fb05c824c2c3c1ed3
Andrii Kholod <kholod7angel@gmail.com>
2026-09-22 20:37:23 +0300

    Complete complementary task var2 - indexing of vector elements

--- LW2/LW2_var2_Andrii_Kholod.m
+++ LW2/LW2_var2_Andrii_Kholod.m
@@ -1,3 +1,4 @@
+clear, clc
 a = (-pi/2 : 0.5 : 3*pi)';
 b = a.^2;
 c = sin(a + b);
@@ -5,8 +6,8 @@ disp(c') %[output:669de4d2]
 Z = rand(3, 3);
 Z(2, :) = [];
 Z_transposed = Z';
-t = 0 : 0.002 : 1.5;
 
+t = 0 : 0.002 : 1.5;
 A = 4; 
 f = 3; 
 sigma = 1; 
@@ -14,9 +15,7 @@ U1 = 2.5;
 U2 = 1.5;
 
 s = A * sin(2 * pi * f * t);
-
 n = sigma * randn(size(t));
-
 signal = s + n;
 
 selected_samples = signal(signal > U1);
@@ -27,11 +26,29 @@ num_selected = length(selected_samples);
 min_voltage = min(filtered_signal);
 max_voltage = max(filtered_signal);
 
+A = input('Would you be so kind as to input a 10-element vector: A = ') %[output:59a22573]
+a1 = A(end:-1:6);
+logical_arr = 1:length(A) <= 5;
+a2 = A(logical_arr);
+B = [a1, a2];
+disp('vector B is:') %[output:719795cb]
+disp(B) %[output:3f9135cd]
+
+
 %[appendix]{"version":"1.0"}
 %---
 %[metadata:view]
-%   data: {"layout":"onright"}
+%   data: {"layout":"onright","rightPanelPercent":31.6}
 %---
 %[output:669de4d2]
-%   data: {"dataType":"text","outputData":{"text":"    0.7812    0.0757   -0.2425   -0.0657    0.5757    0.9755   -0.3243   -0.5909    0.8887   -0.8709    0.4963    0.4954   -0.8846   -0.8145   -0.3410   -0.2415   -0.5972   -0.9994   -0.2081    0.9933   -0.8081    0.6406   -0.8132    0.9911   -0.1820   -1.0000   -0.6321   -0.2927   -0.3985   -0.8535   -0.8447    0.5702    0.4095   -0.8138    0.8302   -0.4866   -0.4466    0.9347    0.6917    0.0934   -0.0767    0.2502    0.8831    0.6665   -0.8913    0.2843    0.0237    0.1636   -0.7529    0.8917    0.5538   -0.3585   -0.7312   -0.6988   -0.2253    0.7306    0.7004   -0.9553    0.6252   -0.5483    0.8334   -0.9495   -0.1527    0.8836    0.9691    0.8842    0.9753    0.8574   -0.2309   -0.9108    0.8992   -0.6740    0.7587   -0.9961    0.5115    0.8842    0.0648   -0.4395\n\n","truncated":false}}
+%   data: {"dataType":"text","outputData":{"text":"    0.7812    0.0757   -0.2425   -0.0657    0.5757    0.9755   -0.3243   -0.5909    0.8887   -0.8709    0.4963    0.4954   -0.8846   -0.8145   -0.3410   -0.2415   -0.5972   -0.9994   -0.2081    0.9933   -0.8081    0.6406\n\n","truncated":false}}
+%---
+%[output:59a22573]
+%   data: {"dataType":"matrix","outputData":{"columns":10,"name":"A","rows":1,"type":"double","value":[["1","2","3","4","5","6","7","8","9","10"]]}}
+%---
+%[output:719795cb]
+%   data: {"dataType":"text","outputData":{"text":"vector B is:\n","truncated":false}}
+%---
+%[output:3f9135cd]
+%   data: {"dataType":"text","outputData":{"text":"    10     9     8     7     6     1     2     3     4     5\n\n","truncated":false}}
 %---
